782371a65df412ce32ca52409a7e977d32cf4a8a
mertyucesoy <yucesoymert@hotmail.com>
2025-08-08 14:30:25 +0300

    Fix EmailBackend

--- tennis_reservation/backends.py
+++ tennis_reservation/backends.py
@@ -2,10 +2,10 @@ from django.contrib.auth.backends import ModelBackend
 from court.models import CustomUser
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = CustomUser.objects.get(email=email)
-            if user.check_password(password):
+            user = CustomUser.objects.get(email=username)
+            if user.check_password(password) and self.user_can_authenticate(user):
                 return user
         except CustomUser.DoesNotExist:
             return None
