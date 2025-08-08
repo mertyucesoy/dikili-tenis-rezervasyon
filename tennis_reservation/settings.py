e8fa31f0a312393c6a3e85fc50786dc8819f8dfc
mertyucesoy <yucesoymert@hotmail.com>
2025-08-08 10:21:21 +0300

    Fix static settings for Render deployment

--- tennis_reservation/settings.py
+++ tennis_reservation/settings.py
@@ -7,7 +7,12 @@ SECRET_KEY = 'django-insecure-5af(hnr^^+nl+2g4((^ha1z1#6#q84-k2@y)pd5%^%4$92^e14
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dikili-tenis-rezervasyon.onrender.com', 'dikili-tenis-rezervasyon.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'dikili-tenis-rezervasyon.onrender.com',
+    'dikili-tenis-rezervasyon.pythonanywhere.com',
+]
 
 # Uygulamalar
 INSTALLED_APPS = [
@@ -18,13 +23,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
-
-    'court',  # Senin uygulaman
+    'court',
 ]
 
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # static files için önemli
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -38,7 +43,7 @@ ROOT_URLCONF = 'tennis_reservation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  # Gerekirse burada template klasör yolunu belirt
+        'DIRS': [],  # Gerekirse özel template klasörü yolu verilir
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -60,7 +65,7 @@ DATABASES = {
     }
 }
 
-# Şifre Doğrulama
+# Şifre doğrulama
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -68,21 +73,27 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Uluslararası Ayarlar
+# Uluslararası ayarlar
 LANGUAGE_CODE = 'tr'
 TIME_ZONE = 'Europe/Istanbul'
 USE_I18N = True
 USE_TZ = True
 
-# Statik dosyalar
+# Statik dosyalar (Render için doğru yapı)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # dosyalar buraya toplanacak
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic buraya kopyalar
+
+# STATICFILES_DIRS sadece localde özel dosyaların varsa gereklidir.
+# Eğer kullanmıyorsan tamamen kaldır:
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Varsayılan AutoField
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Giriş ayarları
+# Giriş yönlendirmeleri
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
@@ -90,14 +101,15 @@ LOGOUT_REDIRECT_URL = '/login/'
 # Özel kullanıcı modeli
 AUTH_USER_MODEL = 'court.CustomUser'
 
-# E-posta gönderme ayarları (Gmail SMTP üzerinden doğrulama için)
+# E-posta ayarları
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dikilitenis@gmail.com'  # senin Gmail adresin
-EMAIL_HOST_PASSWORD = 'zyqrwinycmmvyxkb'   # Uygulama şifresi (Google'dan alınan)
+EMAIL_HOST_USER = 'dikilitenis@gmail.com'
+EMAIL_HOST_PASSWORD = 'zyqrwinycmmvyxkb'  # Google uygulama şifresi
 
+# E-posta veya kullanıcı adı ile giriş desteği
 AUTHENTICATION_BACKENDS = [
     'tennis_reservation.backends.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
