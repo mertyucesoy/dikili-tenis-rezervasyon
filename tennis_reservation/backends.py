3c9fe0994a9e8c953396d84f19fd29135629960b
mertyucesoy <yucesoymert@hotmail.com>
2025-08-09 10:08:46 +0300

    Auth: email'i case-insensitive ve trim yap

--- tennis_reservation/backends.py
+++ tennis_reservation/backends.py
@@ -1,11 +1,22 @@
 from django.contrib.auth.backends import ModelBackend
-from court.models import CustomUser
+from django.contrib.auth import get_user_model
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        try:
-            user = CustomUser.objects.get(email=username)
-            if user.check_password(password) and self.user_can_authenticate(user):
-                return user
-        except CustomUser.DoesNotExist:
+        if not username or not password:
             return None
+
+        UserModel = get_user_model()
+        email = username.strip()  # baş/son boşlukları temizle
+
+        try:
+            # email'i büyük/küçük harfe duyarsız ara
+            user = UserModel.objects.get(email__iexact=email)
+        except UserModel.DoesNotExist:
+            return None
+        except UserModel.MultipleObjectsReturned:
+            user = UserModel.objects.filter(email__iexact=email).order_by('id').first()
+
+        if user and user.check_password(password) and self.user_can_authenticate(user):
+            return user
+        return None
