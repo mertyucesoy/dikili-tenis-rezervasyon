dc0120aa6485d94856fcd22440c055d90edbd60c
mertyucesoy <yucesoymert@hotmail.com>
2025-08-09 10:40:34 +0300

    Cleanup: remove temporary admin bootstrap endpoints

--- court/urls.py
+++ court/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('verify/', views.verify_email, name='verify_email'),
-    path('create-admin/', views.create_admin),
 
 
     # ⏳ Geçmiş rezervasyonlar
