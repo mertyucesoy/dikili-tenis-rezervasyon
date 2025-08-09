c12cf021d6586a517a098ff237a4ffbd2f14005b
mertyucesoy <yucesoymert@hotmail.com>
2025-08-09 10:11:44 +0300

    deneme

--- tennis_reservation/backends.py
+++ tennis_reservation/backends.py
@@ -1,22 +1,11 @@
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth import get_user_model
+from court.models import CustomUser
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        if not username or not password:
-            return None
-
-        UserModel = get_user_model()
-        email = username.strip()  # baş/son boşlukları temizle
-
         try:
-            # email'i büyük/küçük harfe duyarsız ara
-            user = UserModel.objects.get(email__iexact=email)
-        except UserModel.DoesNotExist:
+            user = CustomUser.objects.get(email=username)
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+        except CustomUser.DoesNotExist:
             return None
-        except UserModel.MultipleObjectsReturned:
-            user = UserModel.objects.filter(email__iexact=email).order_by('id').first()
-
-        if user and user.check_password(password) and self.user_can_authenticate(user):
-            return user
-        return None
